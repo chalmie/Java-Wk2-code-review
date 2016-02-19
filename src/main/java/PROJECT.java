86bcf45e1601a856d045ce0716617986b2659e13
chalmie <student@epicodus.com>
2016-02-19 08:05:03 -0800

    setup initial files

---
+++ src/main/java/PROJECT.java
@@ -0,0 +1,5 @@
+public class PROJECT {
+  public static void main(String[] args) {}
+
+  
+}
---
+++ src/test/java/WordTest.java
@@ -0,0 +1,10 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class PROJECTTest {
+
+  @Test
+  public void method_input_result() {
+    // eventually put your testing code here
+  }
+}
